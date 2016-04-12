7a0b6a680de3a687512d81b01ae489201b17aad6
beanwonder <shenzhebin8@gmail.com>
2016-04-12 14:59:33 -0500

    bug fix in mem access and compare, refine/remove duplicate code in some sense

--- compilers/L2-spill/spill.ml
+++ compilers/L2-spill/spill.ml
@@ -52,8 +52,9 @@ let spill_in_function f var prefix =
     let spills_n8 = string_of_int (spills * 8) in
     let is_var_to_spill s = (s = var) in
     let counter = ref 0 in
-    let spill_instruction = function
+    let spill_inst = function
       | Expr [Atom w; Atom "<-"; Atom s] as inst ->
+        (* w <- s *)
         if (is_var_to_spill w && is_var_to_spill s) then
           []
         else if is_var_to_spill w then
@@ -62,103 +63,111 @@ let spill_in_function f var prefix =
           Expr [Atom w; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] :: []
         else
           [inst]
-      | Expr [Atom w; Atom "<-"; Expr mem as mem_acc] when is_var_to_spill w ->
-        (* conbined with w <- mem and w <- stack*)
+      | Expr [Atom w; Atom "<-"; Expr [Atom "stack-arg"; _] as stack_acc] when is_var_to_spill w ->
+        (* w <- (stack-arg n8) *)
         let suffix = string_of_int !counter in
-        let var_after_spill = prefix ^ suffix in
+        let var_after_spill = prefix ^ suffix
+        in
         incr counter;
-        Expr [Atom var_after_spill; Atom "<-"; mem_acc] ::
+        Expr [Atom var_after_spill; Atom "<-"; stack_acc] ::
         Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill] :: []
-      | Expr [Expr [(Atom "mem"); Atom x; Atom n8] as mem; Atom "<-"; Atom s] when is_var_to_spill s ->
+      | Expr [Atom w; Atom "<-"; Expr [Atom "mem"; Atom x; Atom n8] as mem]
+        when (is_var_to_spill w || is_var_to_spill x) ->
+        (* w <- (mem x n8)*)
         let suffix = string_of_int !counter in
         let var_after_spill = prefix ^ suffix in
+        let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] in
+        let assign_inst =
+          Expr [if is_var_to_spill w then Atom var_after_spill else Atom w;
+                Atom "<-";
+                if is_var_to_spill x then Expr [Atom "mem"; Atom var_after_spill; Atom n8] else mem] in
+        let mwrite = Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill]
+        in
         incr counter;
-        Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-        Expr [mem; Atom "<-"; Atom var_after_spill] :: []
-      | Expr [Atom w; Atom op; Atom t] as inst when (is_aop op || is_sop op) ->
+        if is_var_to_spill w then
+          mread :: assign_inst :: mwrite :: []
+        else
+          mread :: assign_inst :: []
+      | Expr [Expr [Atom "mem"; Atom x; Atom n8] as mem; Atom "<-"; Atom s]
+        when (is_var_to_spill x || is_var_to_spill s) ->
+        (* (mem x n8) <- s *)
+        let suffix = string_of_int !counter in
+        let var_after_spill = prefix ^ suffix in
+        let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] in
+        let assign_inst =
+          Expr [if is_var_to_spill x then Expr [Atom "mem"; Atom var_after_spill; Atom n8] else mem;
+                Atom "<-";
+                if is_var_to_spill s then Atom var_after_spill else Atom s]
+        in
+        incr counter;
+        mread :: assign_inst :: []
+      | Expr [Atom w; Atom op; Atom t] when ((is_aop op || is_sop op) && (is_var_to_spill w || is_var_to_spill t)) ->
         (* combined with sop and aop *)
         (* assume no invalid input here *)
-        if is_var_to_spill w && is_var_to_spill t then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom var_after_spill; Atom op; Atom var_after_spill] ::
-          Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill] :: []
-        else if is_var_to_spill w then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom var_after_spill; Atom op; Atom t] ::
-          Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill] :: []
-        else if is_var_to_spill t then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom w; Atom op; Atom var_after_spill] :: []
-        else
-          [inst]
-      | Expr [Atom w; Atom "<-"; Atom t1; Atom cmp; Atom t2] as inst ->
         let suffix = string_of_int !counter in
         let var_after_spill = prefix ^ suffix in
-        if is_var_to_spill w || is_var_to_spill t1 || is_var_to_spill t2 then begin
-          incr counter;
-          let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]]
-          and mwrite = Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill]
-          and cmp_inst = Expr [Atom var_after_spill;
-                                   Atom "<-";
-                                   if is_var_to_spill t1 then Atom var_after_spill else Atom t1;
-                                   Atom cmp;
-                                   if is_var_to_spill t2 then Atom var_after_spill else Atom t2]
-          in
-          if is_var_to_spill w then
-            if is_var_to_spill t1 || is_var_to_spill t2 then
-              mread :: cmp_inst :: mwrite :: []
-            else
-              cmp_inst :: mwrite :: []
+        let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] in
+        let op_inst = Expr [if is_var_to_spill w then Atom var_after_spill else Atom w;
+                            Atom op;
+                            if is_var_to_spill t then Atom var_after_spill else Atom t] in
+        let mwrite = Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill]
+        in
+        incr counter;
+        if is_var_to_spill w then
+          if is_var_to_spill t then
+            mread :: op_inst :: mwrite :: []
           else
-            mread :: cmp_inst :: []
-        end
+            op_inst :: mwrite :: []
         else
-          [inst]
-      | Expr [Atom "cjump"; Atom t1; Atom cmp; Atom t2; label1; label2] as inst ->
-        if is_var_to_spill t1 && is_var_to_spill t2 then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom "cjump"; Atom var_after_spill; Atom cmp; Atom var_after_spill; label1; label2] :: []
-        else if is_var_to_spill t1 then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom "cjump"; Atom var_after_spill; Atom cmp; Atom t2; label1; label2] :: []
-        else if is_var_to_spill t2 then
-          let suffix = string_of_int !counter in
-          let var_after_spill = prefix ^ suffix in
-          incr counter;
-          Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
-          Expr [Atom "cjump"; Atom t1; Atom cmp; Atom var_after_spill; label1; label2] :: []
+          mread :: op_inst :: []
+      | Expr [Atom w; Atom "<-"; Atom t1; Atom cmp; Atom t2]
+        when (is_var_to_spill w || is_var_to_spill t1 || is_var_to_spill t2) ->
+        (* cmp *)
+        let suffix = string_of_int !counter in
+        let var_after_spill = prefix ^ suffix in
+        let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] in
+        let mwrite = Expr [Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]; Atom "<-"; Atom var_after_spill] in
+        let cmp_inst = Expr [if is_var_to_spill w then Atom var_after_spill else Atom w;
+                             Atom "<-";
+                             if is_var_to_spill t1 then Atom var_after_spill else Atom t1;
+                             Atom cmp;
+                             if is_var_to_spill t2 then Atom var_after_spill else Atom t2]
+        in
+        incr counter;
+        if is_var_to_spill w then
+          if is_var_to_spill t1 || is_var_to_spill t2 then
+            mread :: cmp_inst :: mwrite :: []
+          else
+            cmp_inst :: mwrite :: []
         else
-          [inst]
+          mread :: cmp_inst :: []
+      | Expr [Atom "cjump"; Atom t1; Atom cmp; Atom t2; label1; label2]
+        when (is_var_to_spill t1 || is_var_to_spill t2) ->
+        let suffix = string_of_int !counter in
+        let var_after_spill = prefix ^ suffix in
+        let mread = Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] in
+        let cjump_inst = Expr [Atom "cjump";
+                               if is_var_to_spill t1 then Atom var_after_spill else Atom t1;
+                               Atom cmp;
+                               if is_var_to_spill t2 then Atom var_after_spill else Atom t2]
+        in
+        incr counter;
+        mread :: cjump_inst :: []
       | Expr [Atom c as call; Atom u; Atom nat]
         when ((c = "call" || c = "tail-call") && is_var_to_spill u) ->
         let suffix = string_of_int !counter in
-        let var_after_spill = prefix ^ suffix in
+        let var_after_spill = prefix ^ suffix
+        in
         incr counter;
         Expr [Atom var_after_spill; Atom "<-"; Expr [Atom "mem"; Atom "rsp"; Atom spills_n8]] ::
         Expr [call; Atom var_after_spill; Atom nat] :: []
       | _ as inst -> [inst]
-                     (* assume no invalid input here*)
+      (* assume no invalid input here*)
     in
-    l :: ag :: Atom (string_of_int (spills + 1))
-    :: List.fold_left List.append [] (List.map spill_instruction rest)
+    let spill_and_acc acc inst = List.append acc (spill_inst inst) in
+    l :: ag :: Atom (string_of_int (spills + 1)) :: List.fold_left spill_and_acc [] rest
   | _ -> failwith "l2-spill: error: not a valid function"
   end
-
 
 let spill func_sexpr var prefix =
   match func_sexpr with
