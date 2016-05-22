19bc66ac06e2c85af3b9fc2012fd4b4f9a0adada
beanwonder <shenzhebin8@gmail.com>
2016-05-22 17:52:39 -0500

    l4c TODO: lift arbiray args

---
+++ compilers/L4/AST_l4.ml
@@ -0,0 +1,29 @@
+
+type e = L4Var of string
+       | L4Label of string
+       | L4Num of int64
+       | L4Let of (string * e) * e
+       | L4If of e * e * e
+       | L4Add of e * e
+       | L4Sub of e * e
+       | L4Mul of e * e
+       | L4Less of e * e
+       | L4LessEq of e * e
+       | L4Equal of e * e
+       | L4NumberQo of e
+       | L4AQo of e
+       | L4App of e * e list
+       | L4NewArray of e * e
+       | L4NewTuple of e list
+       | L4Aref of e * e
+       | L4Aset of e * e * e
+       | L4Begin of e * e
+       | L4MakeClosure of string * e
+       | L4ClosureProc of e
+       | L4ClosureVars of e
+       | L4Print of e
+       | L4Read
+
+type f = L4Fun of string * string list * e
+
+type p = L4Prog of e * f list
---
+++ compilers/L4/l4c_test.ml
@@ -0,0 +1,32 @@
+open AST_l3
+open AST_l4
+open L4c
+
+let test_compile_l4_e () =
+  let l4_e0 = L4Var "myVar"
+  and l4_e1 = L4Label ":main"
+  and l4_e2 = L4Add (L4Var "myVar", L4Var "myVar")
+  and l4_e3 = L4NumberQo (L4Num Int64.zero)
+  and l4_e4 = L4Aset (L4Label ":go", L4Num Int64.one, L4Var "tmp")
+  and l4_e5 = L4If (L4Read, L4Num Int64.zero, L4Num Int64.one)
+  in
+  begin
+    assert (compile_l4_e l4_e0 = L3D (L3V (L3Var "l4_myVar")));
+    assert (compile_l4_e l4_e1 = L3D (L3V (L3Label ":main")));
+    assert (compile_l4_e l4_e2 = L3D (L3Add (L3Var "l4_myVar", L3Var "l4_myVar")));
+    assert (compile_l4_e l4_e3 = L3D (L3NumberQo (L3Num Int64.zero))); 
+    assert (compile_l4_e l4_e4 = L3D (L3Aset (L3Label ":go", L3Num Int64.one, L3Var "l4_tmp")));
+    assert (compile_l4_e l4_e5 = L3Let (("l3_var0", L3Read),
+                                        L3If (L3Var "l3_var0",
+                                              L3D (L3V (L3Num Int64.zero)),
+                                              L3D (L3V (L3Num Int64.one)))));
+    print_endline "pass compile_l4_e tests"
+  end
+
+let run_tests () =
+  begin
+    test_compile_l4_e ();
+  end
+
+let () =
+  run_tests ()
