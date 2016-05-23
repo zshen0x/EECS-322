2f47053ab0e0f7e681d5c07aabf89084b5184165
beanwonder <shenzhebin8@gmail.com>
2016-05-23 09:02:00 -0500

    bug fix in compile let case

--- compilers/L4/l4c.ml
+++ compilers/L4/l4c.ml
@@ -9,70 +9,66 @@ and l4_var_prefix = "l4_"
 
 (* top-level function *)
 
-let get_fresh_l3_var =
-  let l3_var_cnt = ref 0 in
-  fun () ->
-    let l3_var = l3_var_prefix ^ string_of_int !l3_var_cnt in
-    begin
-      incr l3_var_cnt;
-      l3_var
-    end
-
 let l4_var_prefixer l4_var = l4_var_prefix ^ l4_var
 
 let is_l3_val = function
   | L3V _ -> true
   | _ -> false
 
-let maybe_let d f =
-  match d with
-  | L3V v -> f d
-  | _ ->
-    let x = get_fresh_l3_var () in
-    L3Let ((x, d), (f (L3V (L3Var x))))
-
 (* l4_e k -> l3_e *)
 (* fill d k = k d so eliminate fill*)
-let rec norm_rec l4_e k =
-  match l4_e with
-  | L4Var id -> k (L3D (L3V (L3Var (l4_var_prefixer id)))) (* variable renaming *)
-  | L4Label id -> k (L3D (L3V (L3Label id)))
-  | L4Num n -> k (L3D (L3V (L3Num n)))
-  | L4Let ((var_str, bind_e), body_e) ->
-    norm_rec bind_e
-      (function
-        | L3D d ->
-          maybe_let d
-            (fun d -> L3Let ((var_str, d), norm_rec body_e k))
-        | _ -> invalid_arg "bind expected to be a d")
-  | L4If (cond_e, thn_e, els_e) ->
-    norm_rec cond_e
-      (function
-        | L3D d ->
-          maybe_let d
-            (function
-              | L3V cond_v -> L3If (cond_v, (norm_rec thn_e k), (norm_rec els_e k))
-              | _ -> invalid_arg "cond expected to be a v")
-        | _ -> invalid_arg "cond expected to be a d")
-  | L4Add (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Add (l, r))
-  | L4Sub (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Sub (l, r))
-  | L4Mul (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Mul (l, r))
-  | L4Less (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Less (l, r))
-  | L4LessEq (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3LessEq (l, r))
-  | L4Equal (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Equal (l, r))
-  | L4NumberQo any_e -> norm_unary_k any_e k (fun v -> L3NumberQo v)
-  | L4AQo any_e -> norm_unary_k any_e k (fun v -> L3AQo v)
-  | L4App (fun_e, args_es) -> L3D (L3V (L3Label "to do arbitray args"))
-  | L4NewArray (size_e, init_e) -> norm_biop_k size_e init_e k (fun l r -> L3NewArray (l, r))
-  | L4NewTuple vals_es -> L3D (L3V (L3Label "to do arbitray args"))
-  | L4Aref (arr_e, pos_e) -> norm_biop_k arr_e pos_e k (fun l r -> L3Aref (l, r))
-  | L4Aset (arr_e, pos_e, val_e) -> norm_ternary_k arr_e pos_e val_e k (fun x y z -> L3Aset (x, y, z))
-  | L4Begin (fst_e, snd_e) -> norm_rec (L4Let ((get_fresh_l3_var (), fst_e), snd_e)) k
-  | L4MakeClosure (labl, v_e) -> norm_unary_k v_e k (fun v -> L3MakeClosure (labl, v))
-  | L4ClosureProc arr_e -> norm_unary_k arr_e k (fun v -> L3ClosureProc v)
-  | L4ClosureVars arr_e -> norm_unary_k arr_e k (fun v -> L3ClosureVars v)
-  | L4Print v_e -> norm_unary_k v_e k (fun v -> L3Print v)
-  | L4Read -> k (L3D L3Read)
+let compile_l4_e l4_e =
+  let get_fresh_l3_var =
+    let l3_var_cnt = ref 0 in
+    fun () ->
+      let l3_var = l3_var_prefix ^ string_of_int !l3_var_cnt in
+      begin
+        incr l3_var_cnt;
+        l3_var
+      end
+  in
+  let maybe_let d f =
+    match d with
+    | L3V v -> f d
+    | _ ->
+      let x = get_fresh_l3_var () in
+      L3Let ((x, d), (f (L3V (L3Var x))))
+  in
+  let rec norm_rec l4_e k =
+    match l4_e with
+    | L4Var id -> k (L3D (L3V (L3Var (l4_var_prefixer id)))) (* variable renaming *)
+    | L4Label id -> k (L3D (L3V (L3Label id)))
+    | L4Num n -> k (L3D (L3V (L3Num n)))
+    | L4Let ((var_str, bind_e), body_e) ->
+      norm_let_k (l4_var_prefixer var_str) bind_e body_e k
+    | L4If (cond_e, thn_e, els_e) ->
+      norm_rec cond_e
+        (function
+          | L3D d ->
+            maybe_let d
+              (function
+                | L3V cond_v -> L3If (cond_v, (norm_rec thn_e k), (norm_rec els_e k))
+                | _ -> invalid_arg "cond expected to be a v")
+          | _ -> invalid_arg "cond expected to be a d")
+    | L4Add (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Add (l, r))
+    | L4Sub (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Sub (l, r))
+    | L4Mul (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Mul (l, r))
+    | L4Less (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Less (l, r))
+    | L4LessEq (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3LessEq (l, r))
+    | L4Equal (lhs_e, rhs_e) -> norm_biop_k lhs_e rhs_e k (fun l r -> L3Equal (l, r))
+    | L4NumberQo any_e -> norm_unary_k any_e k (fun v -> L3NumberQo v)
+    | L4AQo any_e -> norm_unary_k any_e k (fun v -> L3AQo v)
+    | L4App (fun_e, args_es) -> L3D (L3V (L3Label "to do arbitray args"))
+    | L4NewArray (size_e, init_e) -> norm_biop_k size_e init_e k (fun l r -> L3NewArray (l, r))
+    | L4NewTuple vals_es -> L3D (L3V (L3Label "to do arbitray args"))
+    | L4Aref (arr_e, pos_e) -> norm_biop_k arr_e pos_e k (fun l r -> L3Aref (l, r))
+    | L4Aset (arr_e, pos_e, val_e) -> norm_ternary_k arr_e pos_e val_e k (fun x y z -> L3Aset (x, y, z))
+    | L4Begin (fst_e, snd_e) -> norm_let_k (get_fresh_l3_var ()) fst_e snd_e k
+    | L4MakeClosure (labl, v_e) -> norm_unary_k v_e k (fun v -> L3MakeClosure (labl, v))
+    | L4ClosureProc arr_e -> norm_unary_k arr_e k (fun v -> L3ClosureProc v)
+    | L4ClosureVars arr_e -> norm_unary_k arr_e k (fun v -> L3ClosureVars v)
+    | L4Print v_e -> norm_unary_k v_e k (fun v -> L3Print v)
+    | L4Read -> k (L3D L3Read)
 (*
 and norm_biop_k lhs_e rhs_e k what =
     norm_rec lhs_e
@@ -93,7 +89,7 @@ and norm_biop_k lhs_e rhs_e k what =
               | _ -> invalid_arg "lhs expected to be a v")
         | _ -> invalid_arg "lhs expected to be a d")
 *)
-and norm_unary_k any_e k what =
+  and norm_unary_k any_e k what =
     norm_rec any_e
       (function
         | L3D d ->
@@ -102,31 +98,34 @@ and norm_unary_k any_e k what =
               | L3V v -> k (L3D (what v))
               | _ -> invalid_arg "expected to be a v")
         | _ -> invalid_arg "expected to be a d")
-and norm_biop_k lhs_e rhs_e k what =
-  norm_rec lhs_e
-    (function
-      | L3D d ->
-        maybe_let d
-          (function
-            | L3V v -> norm_unary_k rhs_e k (what v)
-            | _ -> invalid_arg "expected to be a v")
-      | _ -> invalid_arg "expected to be a d")
-and norm_ternary_k fst snd thd k what =
-  norm_rec fst
-    (function
-      | L3D d ->
-        maybe_let d
-          (function
-            | L3V v -> norm_biop_k snd thd k (what v)
-            | _ -> invalid_arg "expected to v")
-      | _ -> invalid_arg "exptected to be a d")
-
-
-(* l3_d context -> l3_e *)
-
-let compile_l4_e l4_e =
+  and norm_biop_k lhs_e rhs_e k what =
+    norm_rec lhs_e
+      (function
+        | L3D d ->
+          maybe_let d
+            (function
+              | L3V v -> norm_unary_k rhs_e k (what v)
+              | _ -> invalid_arg "expected to be a v")
+        | _ -> invalid_arg "expected to be a d")
+  and norm_ternary_k fst snd thd k what =
+    norm_rec fst
+      (function
+        | L3D d ->
+          maybe_let d
+            (function
+              | L3V v -> norm_biop_k snd thd k (what v)
+              | _ -> invalid_arg "expected to v")
+        | _ -> invalid_arg "exptected to be a d")
+  and norm_let_k var_str bind_e body_e k =
+    norm_rec bind_e
+      (function
+        | L3D d ->
+          L3Let ((var_str, d), norm_rec body_e k)
+        | _ -> invalid_arg "bind expected to be a d")
+  in
   let norm e = norm_rec e (fun e -> e) in
   norm l4_e
+
 
 let compile_l4_f = function
   | L4Fun (label, args, body_e) ->
@@ -135,5 +134,3 @@ let compile_l4_f = function
 let compile_l4_p = function
   | L4Prog (prog_e, fundefs) ->
     L3Prog (compile_l4_e prog_e, List.map compile_l4_f fundefs)
-
-
--- compilers/L4/l4c_test.ml
+++ compilers/L4/l4c_test.ml
@@ -9,6 +9,13 @@ let test_compile_l4_e () =
   and l4_e3 = L4NumberQo (L4Num Int64.zero)
   and l4_e4 = L4Aset (L4Label ":go", L4Num Int64.one, L4Var "tmp")
   and l4_e5 = L4If (L4Read, L4Num Int64.zero, L4Num Int64.one)
+  and l4_e6 = L4Begin (L4Read, L4Print (L4Num Int64.one))
+  and l4_e7 = L4Let (("myVar", L4Num Int64.zero),
+                     L4Add (L4Var "myVar", L4Var "myVar"))
+  and l4_e8 = L4Let (("myVar", L4Num Int64.zero),
+                     L4Num Int64.zero)
+  and l4_e9 = L4Begin (L4Num Int64.zero, L4Num Int64.one)
+  and l4_e10 = L4Let (("tmp", L4Num Int64.zero), L4Print (L4Num Int64.one))
   in
   begin
     assert (compile_l4_e l4_e0 = L3D (L3V (L3Var "l4_myVar")));
@@ -20,8 +27,19 @@ let test_compile_l4_e () =
                                         L3If (L3Var "l3_var0",
                                               L3D (L3V (L3Num Int64.zero)),
                                               L3D (L3V (L3Num Int64.one)))));
+    assert (compile_l4_e l4_e6 = L3Let (("l3_var0", L3Read),
+                                        L3D (L3Print (L3Num Int64.one))));
+    assert (compile_l4_e l4_e7 = L3Let (("l4_myVar", L3V (L3Num (Int64.zero))),
+                                        L3D (L3Add (L3Var "l4_myVar", L3Var "l4_myVar"))));
+    assert (compile_l4_e l4_e8 = L3Let (("l4_myVar", L3V (L3Num Int64.zero)),
+                                        L3D (L3V (L3Num Int64.zero))));
+    assert (compile_l4_e l4_e9 = L3Let (("l3_var0", L3V (L3Num Int64.zero)),
+                                        L3D (L3V (L3Num Int64.one))));
+    assert (compile_l4_e l4_e10 = L3Let (("l4_tmp", L3V (L3Num Int64.zero)),
+                                         L3D (L3Print (L3Num Int64.one))));
     print_endline "pass compile_l4_e tests"
   end
+
 
 let run_tests () =
   begin
