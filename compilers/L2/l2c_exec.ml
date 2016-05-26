9497a3808f28c7b37b4975811b0f2b6abaf61dbf
beanwonder <shenzhebin8@gmail.com>
2016-05-25 19:25:40 -0500

    l2exec

---
+++ compilers/L2/l2c_exec.ml
@@ -0,0 +1,12 @@
+open SExpr
+open L2c
+
+let () =
+  match Sys.argv with
+  | [|_; filename|] ->
+    let print_result = function
+      | Some l2_prog -> SExpr.print_sexpr_indent [l2_prog]
+      | None -> print_endline "\"could not register allocate\""
+    in
+    List.iter (fun p -> print_result @@ compile_program p) (parse_file filename)
+  | _ -> ()
